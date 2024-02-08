6881fcb8816ea2d7c63a228ccfdac0d04d2f3701
Bryce Coon <bryce.coon@students.snow.edu>
2024-02-08 10:48:39 -0700

    changed thing

--- web/Program.cs
+++ web/Program.cs
@@ -18,6 +18,17 @@ builder.Services.AddSingleton<UserService>();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.WebHost.ConfigureAppConfiguration((ctx, cb) =>
+{
+    if (!ctx.HostingEnvironment.IsDevelopment())
+    {
+        StaticWebAssetsLoader.UseStaticWebAssets(
+      ctx.HostingEnvironment,
+      ctx.Configuration);
+    }
+}
+);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
